93d2e74a78807fcc7dce91bc9d7e0e7d47ac1b98
SamLoontjens <93867680+SamLoontjens@users.noreply.github.com>
2022-12-23 16:12:25 +0100

    testing stuff

---
+++ R/test stuff.R
@@ -0,0 +1,15 @@
+#' Test stuff.
+#'
+#' @description Test.
+#' @author Sam Loontjens
+#' @export
+#' @return Nothing
+#' @examples
+#'
+test_import <- function(){
+
+
+  #made the directories
+}
+
+
